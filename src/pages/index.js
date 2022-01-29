08aefe4e7b355783aff7be0d8fd7950ca36fe79f
Cody Wellman <zagdrath@member.fsf.org>
2022-01-28 23:38:57 -0500

    Change stuff iunno

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 // Imports
 import * as React from 'react'
+import { Link } from 'gatsby'
 import { Helmet } from 'react-helmet'
 import Header from "../components/header"
 import Footer from "../components/footer"
 import { StaticImage } from 'gatsby-plugin-image'
-import { container, main, avatar, mainHey, mainAboutTitle, mainAbout, } from './index.module.scss'
+import { container, main, avatar, mainHey, mainAboutTitle, mainAbout, mainButtons, button, } from './index.module.scss'
 
 // Define component
 const IndexPage = () => {
@@ -20,6 +21,9 @@ const IndexPage = () => {
           <h1 className={mainHey}>Hey! I'm Zagdrath 👋</h1>
           <h2 className={mainAboutTitle}>I'm a software developer, information technology specialist & </h2>
           <p className={mainAbout}>Welcome! It's great to have you here. This is a blog I created to write some technology tutorials and other stuff.</p>
+          <div className={mainButtons}>
+          <Link to="/blog" className={button}>See my Blog Here</Link>
+          </div>
         </div>
         <div className={avatar}>
           <StaticImage alt="Avatar" src="../images/avatar.png"/>
